8d84d58d71099d7a80d241c855191b1311c5323a
Timur <timurtokaev.toombez@gmail.com>
2023-04-24 01:37:35 +0300

    Add database scripts

---
+++ apps/discord-bot/src/database.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClient = new PrismaClient()
+
+export async function addUser(discordUserId: string) {
+    await prismaClient.$connect()
+
+    await prismaClient.user.create({
+        data: {
+            id: discordUserId,
+        }
+    })
+
+    await prismaClient.$disconnect()
+}
+
+export async function getClicks(discordUserId: string) {
+    await prismaClient.$connect()
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            id: {
+                equals: discordUserId,
+            }
+        }
+    })
+
+    await prismaClient.$disconnect()
+
+    return user?.clicks
+}
+
+export async function updateClicks(discordUserId: string, clicks: number) {
+    await prismaClient.$connect()
+
+    await prismaClient.user.update({
+        data: {
+            clicks: clicks,
+        },
+        where: {
+            id: discordUserId,
+        }
+    })
+
+    await prismaClient.$disconnect()
+}
