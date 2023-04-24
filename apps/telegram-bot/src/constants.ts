198244b5a02b697eff35a6c20c8c915db1e2f512
Timur <timurtokaev.toombez@gmail.com>
2023-04-24 16:07:02 +0300

    Add buttons example

--- apps/telegram-bot/src/constants.ts
+++ apps/telegram-bot/src/constants.ts
@@ -5,3 +5,9 @@ dotenv.config()
 
 // Токен для бота из переменных среды
 export const TOKEN = process.env.TELEGRAM_TOKEN!
+
+// Константа для кнопки кликов
+export const GET_POINTS_BUTTON_ID = 'get-points-button-id'
+
+// Константа для кнопки информации
+export const INFO_BUTTON_ID = 'info-button-id'
--- apps/telegram-bot/src/examples/basic.ts
+++ apps/telegram-bot/src/examples/basic.ts
@@ -1,13 +1,55 @@
-import { Telegraf } from 'telegraf'
-import { TOKEN } from '../constants'
+import { Telegraf, Markup } from 'telegraf'
+import { GET_POINTS_BUTTON_ID, INFO_BUTTON_ID, TOKEN } from '../constants'
 
 // Создание бота
 const client = new Telegraf(TOKEN)
 
 // Обработчик для команды "start"
+// Каждый обработчик может быть асинхронным для того, чтобы последовательно
+// обрабатывать команду
+// Каждый обработчик может содержать параметр контекста (Обычно называют ctx), в котором
+// хранимтся информация о том, где, от кого была вызвана команда, нажата кнопки и т.п.
 client.command('start', (ctx) => {
     // Ответ на вызов команды
     ctx.reply('You have started chat with bot!')
+})
+
+// Обработчик команды "buttons"
+client.command('buttons', async (ctx) => {
+    // Отправка сообщения с кнопками
+    await client.telegram.sendMessage(ctx.chat.id, 'Button', {
+        reply_markup: {
+            // Создание клавиатуры в чате
+            inline_keyboard: [
+                [
+                    // Кнопка-ссылка. Получает текст, который будет отображать
+                    // и ссылку страницы, на которую ведет кнопка
+                    Markup.button.url('google', 'https://google.com'),
+                ],
+                [
+                    // Кнопка с привязкой обработчика. Получает текст, который
+                    // будет отображаться и ID, по которому будем находить обрабатывать
+                    // нажатие на кнопку.
+                    Markup.button.callback('click me', GET_POINTS_BUTTON_ID),
+                    Markup.button.callback('info', INFO_BUTTON_ID),
+                ]
+            ],
+            resize_keyboard: true,
+        }
+    })
+})
+
+// Обработка кнопки "info"
+client.action(INFO_BUTTON_ID, (ctx) => {
+    // Получение ID чата, в котором нажали кнопку
+    const chatId = ctx.chat?.id
+
+    if (!chatId) {
+        return
+    }
+
+    // Отправка сообщение в чат, используя API телеграма
+    client.telegram.sendMessage(chatId, 'Bot\'s info')
 })
 
 // Запуск бота
